d7883c893d5ba41d659a582a3390d3d8c5ecd0fb
HoriaAnton <horiaantonio@gmail.com>
2025-02-16 18:15:33 +0200

    update

--- keyboards-api/obj/Debug/net8.0/keyboards-api.AssemblyInfo.cs
+++ keyboards-api/obj/Debug/net8.0/keyboards-api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("keyboards-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d570a4aae4512376cf70c211f1e94e7c273d5ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("keyboards-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("keyboards-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
